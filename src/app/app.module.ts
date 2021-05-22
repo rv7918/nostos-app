2e6dae1e8135f0ddce0b34b99d4e777235bc7e18
Rav Sohal <ravinder.sohal@gmail.com>
2021-05-22 14:42:10 +0100

    folder structure change for app.component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from './components/app.component';
 import { ListComponent } from './components/list/list.component';
 import { DetailsComponent } from './components/details/details/details.component';
 
