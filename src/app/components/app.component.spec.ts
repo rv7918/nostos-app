3f424ca581d4c3fa372b562ac1024608df4051bb
Rav Sohal <ravinder.sohal@gmail.com>
2021-05-24 00:37:48 +0100

    app.component tests

--- src/app/components/app.component.spec.ts
+++ src/app/components/app.component.spec.ts
@@ -26,10 +26,17 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('nostos-app');
   });
 
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement;
-  //   expect(compiled.querySelector('.content span').textContent).toContain('nostos-app app is running!');
-  // });
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.navbar-brand').textContent).toContain('NHA');
+  });
+
+  it('should render navbar text', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.navbar-text').textContent).toContain('Example app for Nostos Genomics');
+  });
 });
--- src/app/components/app.component.ts
+++ src/app/components/app.component.ts
@@ -7,4 +7,6 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'nostos-app';
+
+  navText = 'Example app for Nostos Genomics';
 }
