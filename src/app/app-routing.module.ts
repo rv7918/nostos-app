f686fb1b0dae0585337176b99d42ac3b99746c0d
Rav Sohal <ravinder.sohal@gmail.com>
2021-05-25 02:22:14 +0100

    Readme updated and wildcard route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,8 @@ import  { DetailsComponent } from './components/details/details/details.componen
 const routes: Routes = [
   { path: '', component: ListComponent},
   { path: 'home', component: ListComponent},
-  { path: 'details/:i', component: DetailsComponent}
+  { path: 'details/:i', component: DetailsComponent}, 
+  { path: '**', component: ListComponent },
 ];
 
 @NgModule({
